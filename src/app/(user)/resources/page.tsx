1c6e5047a20927056eb5521500c41fb718c0fed9
julfiker755 <julfiker755.bd@gmail.com>
2025-11-06 17:53:13 +0600

    config file remove

--- src/app/(user)/resources/page.tsx
+++ src/app/(user)/resources/page.tsx
@@ -1,13 +1,10 @@
 import TabBox from "@/components/reuseable/tab-box";
 import { TabsContent } from "@/components/ui";
-<<<<<<< HEAD
 import {
   Blogs,
   Partnership,
   Podcast,
 } from "@/components/view/user/simple/resources-tab";
-=======
->>>>>>> 10431b705134f9a8036a04c66323d975365de2ec
 
 export default function Resources() {
   return (
@@ -15,7 +12,6 @@ export default function Resources() {
       <TabBox
         defaultValue="blogs"
         tabItem={["Blogs", "Podcast", "Partnership"]}
-<<<<<<< HEAD
         className="flex justify-start w-fit mt-5"
         tabStyle="border-b border-transparent text-lg data-[state=active]:border-primary! data-[state=active]:border-b! data-[state=active]:text-primary"
       >
@@ -27,18 +23,6 @@ export default function Resources() {
         </TabsContent>
         <TabsContent value="partnership">
           <Partnership />
-=======
-        className="flex justify-start w-fit"
-      >
-        <TabsContent value="blogs">
-          <h1>Blogs</h1>
-        </TabsContent>
-        <TabsContent value="podcast">
-          <h1>Podcast</h1>
-        </TabsContent>
-        <TabsContent value="partnership">
-          <h1>Partnership</h1>
->>>>>>> 10431b705134f9a8036a04c66323d975365de2ec
         </TabsContent>
       </TabBox>
     </div>
